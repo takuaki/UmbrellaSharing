7d7bdb5ac0903afcac0b30c632903ea126e8b86e
Takuaki Mori <tsgmacbeg@me.com>
2016-04-09 21:37:36 +0900

    get location information from the current information.

--- app/src/main/java/umbrella/tokyo/jp/umbrella/MainActivity.java
+++ app/src/main/java/umbrella/tokyo/jp/umbrella/MainActivity.java
@@ -4,12 +4,13 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.PermissionChecker;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.Menu;
@@ -36,15 +37,19 @@ import umbrella.tokyo.jp.umbrella.http.WeatherHttp;
 import umbrella.tokyo.jp.umbrella.util.LogUtil;
 import umbrella.tokyo.jp.umbrella.util.WeatherJson;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback , Callback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback , Callback,LocationListener{
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int zoom = 15;
+    private static long  minTime = 1000*10l;
+    private static float minDistance = 5;
+
     private SupportMapFragment mMapFragment = null;
     private  GoogleMap mMap =null;
     private CardView mWeatherCard = null;
     private LocationManager locationManager;
     private WeatherHttp weatherHttp = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +57,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         weatherHttp = new WeatherHttp(this);
+        mWeatherCard = (CardView)findViewById(R.id.weather_card);
+        mMapFragment =(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
 
         FloatingActionButton fabMyLocation = (FloatingActionButton) findViewById(R.id.fab_mylocation);
         fabMyLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setMyLocation(getBaseContext());
+                Location location = getLastKnownLocation();
+                setMyLocation(location);
             }
         });
         FloatingActionButton fabCalling = (FloatingActionButton)findViewById(R.id.fab_call);
@@ -68,26 +77,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-        mWeatherCard = (CardView)findViewById(R.id.weather_card);
-        mMapFragment =(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-        mMapFragment.getMapAsync(this);
-
     }
 
+    /**
+     * カメラ更新
+     * @param googleMap
+     */
     @Override
-    public void onMapReady(GoogleMap googleMap){
+    public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        setMyLocation(this);
+        Location location = getLastKnownLocation();
+        setMyLocation(location);
     }
 
     @Override
     protected void onResume() {
+        LogUtil.d(TAG,"onResume");
         super.onResume();
-        if(mMap!=null){
-            setMyLocation(this);
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if(checkPermission()){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,minTime,minDistance,this);
         }
     }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(checkPermission()){
+            locationManager.removeUpdates(this);
+        };
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,13 +136,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        LogUtil.d(TAG,"onLocationChanged location "+(location!=null));
+        setMyLocation(location);
+    }
 
-    private void setMyLocation(@NonNull Context context){
-        if(PermissionChecker.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            throw new RuntimeException("permission denied : ACCESS_FILE_LOCATION");
-        }
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        Location location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LogUtil.d(TAG,"onStatusChanged");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        LogUtil.d(TAG,"onProviderEnabled");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        LogUtil.d(TAG,"onProviderDisabled");
+    }
+
+
+
+    private void setMyLocation(@Nullable Location location){
+        if(location==null) return ;
         LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
         weatherHttp.onLoad(location,this);
         //map animation
@@ -124,6 +168,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
     }
 
+    private Location getLastKnownLocation(){
+        if(!checkPermission()) return null;
+        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
+    private boolean checkPermission(){
+        if(locationManager!=null){
+            if((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)||
+                    ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
 
     //weather callback
     @Override
